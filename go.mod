389af3c1103640d4a367676b86df3e53ab88f033
Rahul-D78 <rahul.kumar@aveshasystems.com>
2022-05-06 12:26:02 +0530

    Licence headers added with proper imports
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- go.mod
+++ go.mod
@@ -1,11 +1,9 @@
-module bitbucket.org/realtimeai/kubeslice-router-sidecar
+module github.com/kubeslice/router-sidecar
 
 go 1.17
 
 require (
 	github.com/lorenzosaino/go-sysctl v0.1.1
-	github.com/pkg/errors v0.9.1
-	github.com/prometheus/common v0.4.0
 	github.com/vishvananda/netlink v1.1.0
 	go.ligato.io/vpp-agent/v3 v3.2.0
 	go.uber.org/zap v1.16.0
@@ -16,11 +14,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
-	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
-	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
@@ -31,6 +25,5 @@ require (
 	golang.org/x/tools v0.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20200601130524-0f60399e6634 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	honnef.co/go/tools v0.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
+/*  Copyright (c) 2022 Avesha, Inc. All rights reserved.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package main
 
 import (
-	"bitbucket.org/realtimeai/kubeslice-router-sidecar/pkg/logger"
-	"bitbucket.org/realtimeai/kubeslice-router-sidecar/pkg/server"
-	sidecar "bitbucket.org/realtimeai/kubeslice-router-sidecar/pkg/sidecar/sidecarpb"
 	"fmt"
-	"google.golang.org/grpc"
 	"net"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+
+	"github.com/kubeslice/router-sidecar/pkg/logger"
+	"github.com/kubeslice/router-sidecar/pkg/server"
+	sidecar "github.com/kubeslice/router-sidecar/pkg/sidecar/sidecarpb"
+	"google.golang.org/grpc"
 )
 
 // startGrpcServer shall start the GRPC server to communicate to Slice Controller
