dde38396cc523b2d0cca7feb8b57ebe22f32d948
Rahul-D78 <rahul.kumar@aveshasystems.com>
2022-10-03 13:55:54 +0530

    observing the loop
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- pkg/server/slicerouter_config.go
+++ pkg/server/slicerouter_config.go
@@ -303,6 +303,7 @@ func sliceRouterInjectRoute(remoteSubnet string, nextHopIPList []string) error {
 	_, routePresent := remoteSubnetRouteMap[remoteSubnet]
 	nextHopIpSlice := []*netlink.NexthopInfo{}
 
+	count := 0
 	for i := 0; i < len(nextHopIPList); i++ {
 
 		gwObj := &netlink.NexthopInfo{Gw: net.ParseIP(nextHopIPList[i])}
@@ -344,6 +345,8 @@ func sliceRouterInjectRoute(remoteSubnet string, nextHopIPList []string) error {
 				}
 			}
 		}
+		count++
+		fmt.Println("val of count and nexthopIp", nextHopIPList[i], i)
 		remoteSubnetRouteMap[remoteSubnet] = append(remoteSubnetRouteMap[remoteSubnet], nextHopIPList[i])
 	}
 	fmt.Println("remoteSubnetRouteMap after loop ends", remoteSubnetRouteMap)
