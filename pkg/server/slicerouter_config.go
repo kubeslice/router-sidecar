570f7eb3ee986b0f3102112564927cee7df4e846
rahulsawra98 <r.sawra@gmail.com>
2022-12-23 18:04:01 +0530

    use FAMILY_ALL

--- pkg/server/slicerouter_config.go
+++ pkg/server/slicerouter_config.go
@@ -438,7 +438,8 @@ func vl3ReconcileRoutesInKernel() error {
 func getNextHopInfoSlice(nextHopIPList []string) ([]*netlink.NexthopInfo, error) {
 	nextHopIpSlice := []*netlink.NexthopInfo{}
 	for _, nextHopIP := range nextHopIPList {
-		installedRoutes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
+		installedRoutes, err := netlink.RouteList(nil, netlink.FAMILY_ALL)
+		logger.GlobalLogger.Info("route","route",installedRoutes)
 		if err != nil {
 			return nil, err
 		}
